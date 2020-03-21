25c999c6b8177e18e89493525671a6aff8269b74
Andrew Dibble <acdibble@gmail.com>
2020-03-21 16:29:26 +0100

    Remove unused code

--- src/channels/getChannelsList.ts
+++
@@ -1,8 +0,0 @@
-import slackRequest from '../requests/slackRequest';
-import { Slack } from '../types';
-
-export default async (): Promise<Slack.Channel[]> => {
-  const { channels } = await slackRequest(Slack.Method.ConversationsList, { httpMethod: 'GET' });
-
-  return channels;
-};
