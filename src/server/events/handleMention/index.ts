512fca612eebb26fda48159f1c18ad1dee84d0cc
Andrew Dibble <acdibble@gmail.com>
2020-03-22 02:13:50 +0100

    Fix mention code

--- src/server/events/handleMention/index.ts
+++ src/server/events/handleMention/index.ts
@@ -4,13 +4,14 @@ import getCoronavirusUpdate from './getCoronavirusUpdate';
 import getMe from '../../../meta/getMe';
 import splitIntoBlocks from './getCoronavirusUpdate/splitIntoBlocks';
 
-export default async ({ event: { text, channel, user } }: Slack.Payloads.Event): Promise<void> => {
+export default async (
+  { event: { text, channel, user } }: Slack.Payloads.Event,
+): Promise<Slack.Responses.PostMessage> => {
   const trimmed = text.trim();
   let message: Slack.Requests.PostMessage = { channel, text: "I don't know what to do with my hands" };
 
   if (trimmed === `<@${await getMe()}>`) {
     message = { channel, text: `<@${user}>?` };
-    return;
   } if (trimmed.includes('coronavirus update')) {
     const [summary, data] = await getCoronavirusUpdate();
     message = {
@@ -20,5 +21,5 @@ export default async ({ event: { text, channel, user } }: Slack.Payloads.Event):
     await postMessage({ channel, text: summary });
   }
 
-  await postMessage(message);
+  return postMessage(message);
 };
