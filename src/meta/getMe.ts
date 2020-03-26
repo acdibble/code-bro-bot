8032f77e3e714fbac21430710e4daf8b045a76e3
Andrew Dibble <acdibble@gmail.com>
2020-03-26 23:26:14 +0100

    Migrate commands to queue

--- src/meta/getMe.ts
+++ src/meta/getMe.ts
@@ -4,7 +4,6 @@ import { Slack } from '../types';
 export const teamIdToUserIdMap: Record<string, string> = {};
 
 export default async (team: string): Promise<string> => {
-  console.log(teamIdToUserIdMap);
   if (teamIdToUserIdMap[team]) return teamIdToUserIdMap[team];
   const { user_id: me } = await slackRequest(Slack.Method.AuthTest);
   teamIdToUserIdMap[team] = me;
--- src/server/commands/index.ts
+++ src/server/commands/index.ts
@@ -2,18 +2,33 @@ import { Router } from 'express';
 import { Slack, CodeBro } from '../../types';
 import bro from './bro';
 import help from './help';
+import Queue from '../../Queue';
+
+interface CommandEvent {
+  command: string | undefined;
+  requestBody: Slack.Payloads.Command;
+}
+
+const commands = new Queue<CommandEvent>();
 
 export default Router()
   .post('/', async (req: Slack.IncomingRequest<Slack.Payloads.Command>, res) => {
     res.end();
-    const { text } = req.body;
-    const command = text.match(/^\w+/)?.[0].toLowerCase();
+    const command = req.body.text.match(/^\w+/)?.[0].toLowerCase();
+    commands.enqueue({ command, requestBody: req.body });
+  });
+
+(async () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  for await (const { command, requestBody } of commands) {
     switch (command) {
       case CodeBro.Command.Bro:
-        await bro(req.body);
+        await bro(requestBody);
         break;
       case CodeBro.Command.Help:
       default:
-        await help(req.body.response_url);
+        await help(requestBody.response_url);
     }
-  });
+  }
+})();
