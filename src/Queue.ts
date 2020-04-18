c0c4cf68d25264a79846018530f1a45cc191e69e
Andrew Dibble <acdibble@gmail.com>
2020-04-18 15:22:20 +0200

    Change queue to linked list

--- src/Queue.ts
+++ src/Queue.ts
@@ -1,20 +1,33 @@
 import { EventEmitter } from 'events';
 
+interface EventNode<T> {
+  message: T;
+  next: EventNode<T> | null;
+}
+
 export default class Queue<T = any> extends EventEmitter {
   private nextMessage = Symbol('nextMessage');
 
   private isReady = Symbol('isReady');
 
-  private messages: T[] = [];
+  private head: EventNode<T> | null = null;
+
+  private tail: EventNode<T> | null = null;
 
   // should be fixed in ts 3.9 https://github.com/microsoft/TypeScript/issues/37564
   /* eslint-disable @typescript-eslint/ban-ts-ignore, no-await-in-loop */
   // @ts-ignore
   async* [Symbol.asyncIterator](): AsyncGenerator<T, never, Promise<undefined>> {
     while (true) {
-      for (let message = this.messages.shift(); message !== undefined; message = this.messages.shift()) {
+      let current = this.head;
+      while (current !== null) {
+        this.head = current.next;
+        if (this.head === null) {
+          this.tail = null;
+        }
         // @ts-ignore
-        await (yield message);
+        await (yield current.message);
+        current = this.head;
       }
 
       this.emit(this.isReady);
@@ -28,8 +41,16 @@ export default class Queue<T = any> extends EventEmitter {
   /* eslint-enable @typescript-eslint/ban-ts-ignore, no-await-in-loop */
 
   public enqueue(message: T): this {
-    if (!this.emit(this.nextMessage, message)) {
-      this.messages.push(message);
+    if (this.emit(this.nextMessage, message)) {
+      return this;
+    }
+
+    if (this.head === null || this.tail === null) {
+      this.head = { message, next: null };
+      this.tail = this.head;
+    } else {
+      this.tail.next = { message, next: null };
+      this.tail = this.tail.next;
     }
 
     return this;
