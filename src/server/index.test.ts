5511fda24ce6125945340a327ede1129875e5871
Andrew Dibble <acdibble@gmail.com>
2020-04-18 18:37:29 +0200

    Split lines

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -360,24 +360,84 @@ describe('Server', () => {
                   type: 'section',
                   text: {
                     type: 'mrkdwn',
-                    // eslint-disable-next-line max-len
-                    text: 'If the data is bad, I got it from here https://github.com/CSSEGISandData\nTotal cases: 65778\nTotal deaths: 942\nTotal recovered: 361\nTotal active: 0',
+                    text: 'If the data is bad, I got it from here https://github.com/CSSEGISandData\n'
+                      + 'Total cases: 65778\n'
+                      + 'Total deaths: 942\n'
+                      + 'Total recovered: 361\n'
+                      + 'Total active: 0',
                   },
                 },
                 {
                   type: 'section',
                   text: {
                     type: 'mrkdwn',
-                    // eslint-disable-next-line max-len
-                    text: '```State                   |Last updated|Confirmed cases|Deaths|Recovered|Active cases\n-----------------------------------------------------------------------------------\nNew York                |3/25        |30841          |285   |0        |0\nNew Jersey              |3/25        |4402           |62    |0        |0\nCalifornia              |3/25        |2998           |65    |0        |0\nWashington              |3/25        |2591           |133   |0        |0\nMichigan                |3/25        |2296           |43    |0        |0\nIllinois                |3/25        |1865           |19    |0        |0\nMassachusetts           |3/25        |1838           |15    |0        |0\nLouisiana               |3/25        |1795           |65    |0        |0\nFlorida                 |3/25        |1682           |23    |0        |0\nPennsylvania            |3/25        |1260           |15    |0        |0\nGeorgia                 |3/25        |1247           |40    |0        |0\nTexas                   |3/25        |1229           |15    |0        |0\nColorado                |3/25        |1021           |16    |0        |0\nTennessee               |3/25        |916            |3     |0        |0\nConnecticut             |3/25        |875            |19    |0        |0\nOhio                    |3/25        |704            |11    |0        |0\nWisconsin               |3/25        |621            |7     |0        |0\nNorth Carolina          |3/25        |590            |2     |0        |0\nIndiana                 |3/25        |477            |14    |0        |0\nMaryland                |3/25        |425            |4     |0        |0\nSouth Carolina          |3/25        |424            |7     |0        |0\nArizona                 |3/25        |401            |6     |0        |0\nVirginia                |3/25        |396            |9     |0        |0\nAlabama                 |3/25        |381            |1     |0        |0\nMississippi             |3/25        |377            |3     |0        |0\nMissouri                |3/25        |354            |8     |0        |0\nUtah                    |3/25        |340            |1     |0        |0\nNevada                  |3/25        |323            |6     |0        |0\nMinnesota               |3/25        |286            |1     |0        |0\nArkansas                |3/25        |280            |2     |0        |0\nOregon                  |3/25        |266            |10    |0        |0\nKentucky                |3/25        |197            |5     |0        |0\nDistrict of Columbia    |3/25        |187            |2     |0        |0\nOklahoma                |3/25        |164            |5     |0        |0\nIowa                    |3/25        |146            |1     |0        |0\nMaine                   |3/25        |142            |0     |0        |0\nKansas                  |3/25        |134            |3     |0        |0```',
+                    text: '```State                   |Last updated|Confirmed cases|Deaths|Recovered|Active cases\n'
+                      + '-----------------------------------------------------------------------------------\n'
+                      + 'New York                |3/25        |30841          |285   |0        |0\n'
+                      + 'New Jersey              |3/25        |4402           |62    |0        |0\n'
+                      + 'California              |3/25        |2998           |65    |0        |0\n'
+                      + 'Washington              |3/25        |2591           |133   |0        |0\n'
+                      + 'Michigan                |3/25        |2296           |43    |0        |0\n'
+                      + 'Illinois                |3/25        |1865           |19    |0        |0\n'
+                      + 'Massachusetts           |3/25        |1838           |15    |0        |0\n'
+                      + 'Louisiana               |3/25        |1795           |65    |0        |0\n'
+                      + 'Florida                 |3/25        |1682           |23    |0        |0\n'
+                      + 'Pennsylvania            |3/25        |1260           |15    |0        |0\n'
+                      + 'Georgia                 |3/25        |1247           |40    |0        |0\n'
+                      + 'Texas                   |3/25        |1229           |15    |0        |0\n'
+                      + 'Colorado                |3/25        |1021           |16    |0        |0\n'
+                      + 'Tennessee               |3/25        |916            |3     |0        |0\n'
+                      + 'Connecticut             |3/25        |875            |19    |0        |0\n'
+                      + 'Ohio                    |3/25        |704            |11    |0        |0\n'
+                      + 'Wisconsin               |3/25        |621            |7     |0        |0\n'
+                      + 'North Carolina          |3/25        |590            |2     |0        |0\n'
+                      + 'Indiana                 |3/25        |477            |14    |0        |0\n'
+                      + 'Maryland                |3/25        |425            |4     |0        |0\n'
+                      + 'South Carolina          |3/25        |424            |7     |0        |0\n'
+                      + 'Arizona                 |3/25        |401            |6     |0        |0\n'
+                      + 'Virginia                |3/25        |396            |9     |0        |0\n'
+                      + 'Alabama                 |3/25        |381            |1     |0        |0\n'
+                      + 'Mississippi             |3/25        |377            |3     |0        |0\n'
+                      + 'Missouri                |3/25        |354            |8     |0        |0\n'
+                      + 'Utah                    |3/25        |340            |1     |0        |0\n'
+                      + 'Nevada                  |3/25        |323            |6     |0        |0\n'
+                      + 'Minnesota               |3/25        |286            |1     |0        |0\n'
+                      + 'Arkansas                |3/25        |280            |2     |0        |0\n'
+                      + 'Oregon                  |3/25        |266            |10    |0        |0\n'
+                      + 'Kentucky                |3/25        |197            |5     |0        |0\n'
+                      + 'District of Columbia    |3/25        |187            |2     |0        |0\n'
+                      + 'Oklahoma                |3/25        |164            |5     |0        |0\n'
+                      + 'Iowa                    |3/25        |146            |1     |0        |0\n'
+                      + 'Maine                   |3/25        |142            |0     |0        |0\n'
+                      + 'Kansas                  |3/25        |134            |3     |0        |0```',
                   },
                 },
                 {
                   type: 'section',
                   text: {
                     type: 'mrkdwn',
-                    // eslint-disable-next-line max-len
-                    text: '```Rhode Island            |3/25        |132            |0     |0        |0\nVermont                 |3/25        |125            |8     |0        |0\nDelaware                |3/25        |119            |0     |0        |0\nNew Mexico              |3/25        |113            |1     |0        |0\nNew Hampshire           |3/25        |108            |1     |0        |0\nIdaho                   |3/25        |91             |0     |0        |0\nHawaii                  |3/25        |91             |0     |0        |0\nNebraska                |3/25        |71             |0     |0        |0\nMontana                 |3/25        |65             |0     |0        |0\nPuerto Rico             |3/25        |51             |2     |0        |0\nDiamond Princess        |3/25        |49             |0     |0        |0\nNorth Dakota            |3/25        |45             |0     |0        |0\nWyoming                 |3/25        |44             |0     |0        |0\nAlaska                  |3/25        |41             |1     |0        |0\nSouth Dakota            |3/25        |41             |1     |0        |0\nWest Virginia           |3/25        |39             |0     |0        |0\nGuam                    |3/25        |37             |1     |0        |0\nGrand Princess          |3/25        |28             |1     |0        |0\nVirgin Islands          |3/25        |17             |0     |0        |0\nAmerican Samoa          |3/25        |0              |0     |0        |0\nNorthern Mariana Islands|3/25        |0              |0     |0        |0\nRecovered               |3/25        |0              |0     |361      |0```',
+                    text: '```Rhode Island            |3/25        |132            |0     |0        |0\n'
+                      + 'Vermont                 |3/25        |125            |8     |0        |0\n'
+                      + 'Delaware                |3/25        |119            |0     |0        |0\n'
+                      + 'New Mexico              |3/25        |113            |1     |0        |0\n'
+                      + 'New Hampshire           |3/25        |108            |1     |0        |0\n'
+                      + 'Idaho                   |3/25        |91             |0     |0        |0\n'
+                      + 'Hawaii                  |3/25        |91             |0     |0        |0\n'
+                      + 'Nebraska                |3/25        |71             |0     |0        |0\n'
+                      + 'Montana                 |3/25        |65             |0     |0        |0\n'
+                      + 'Puerto Rico             |3/25        |51             |2     |0        |0\n'
+                      + 'Diamond Princess        |3/25        |49             |0     |0        |0\n'
+                      + 'North Dakota            |3/25        |45             |0     |0        |0\n'
+                      + 'Wyoming                 |3/25        |44             |0     |0        |0\n'
+                      + 'Alaska                  |3/25        |41             |1     |0        |0\n'
+                      + 'South Dakota            |3/25        |41             |1     |0        |0\n'
+                      + 'West Virginia           |3/25        |39             |0     |0        |0\n'
+                      + 'Guam                    |3/25        |37             |1     |0        |0\n'
+                      + 'Grand Princess          |3/25        |28             |1     |0        |0\n'
+                      + 'Virgin Islands          |3/25        |17             |0     |0        |0\n'
+                      + 'American Samoa          |3/25        |0              |0     |0        |0\n'
+                      + 'Northern Mariana Islands|3/25        |0              |0     |0        |0\n'
+                      + 'Recovered               |3/25        |0              |0     |361      |0```',
                   },
                 },
               ],
