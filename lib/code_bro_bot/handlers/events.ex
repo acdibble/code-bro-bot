a2c86cd26d1f08fc79c505ad712a4e40359b82a4
Andrew Dibble <andrew@dibble.codes>
2020-05-08 01:29:20 +0200

    Abandoning report

--- lib/code_bro_bot/handlers/events.ex
+++ lib/code_bro_bot/handlers/events.ex
@@ -21,14 +21,31 @@ defmodule CodeBroBot.Handlers.Events do
   end
 
   @impl true
+  def handle_info({ref, _result}, nil) when is_reference(ref) do
+    {:noreply, nil}
+  end
+
+  @impl true
+  def handle_info({:DOWN, _ref, :process, _pid, _status}, nil) do
+    {:noreply, nil}
+  end
+
+  @impl true
   def handle_cast(
-        %{"event" => %{"type" => "app_mention", "channel" => channel, "text" => text} = body},
+        %{
+          "event" => %{
+            "type" => "app_mention",
+            "channel" => channel,
+            "text" => text,
+            "user" => user
+          }
+        },
         nil
       ) do
     Task.Supervisor.async_nolink(CodeBroBot.Handlers.TaskSupervisor, fn ->
       CodeBroBot.Slack.Request.call(%{
         "channel" => channel,
-        "text" => "I don't know what to do with my hands."
+        "text" => text |> String.trim() |> get_response(user)
       })
     end)
 
@@ -41,16 +58,96 @@ defmodule CodeBroBot.Handlers.Events do
     {:noreply, nil}
   end
 
-  @impl true
-  def handle_info({ref, result}, nil) when is_reference(ref) do
-    IO.puts("Request returned:")
-    IO.inspect(result)
-    {:noreply, nil}
+  defp get_response(text, user) when is_binary(user) do
+    case Regex.match?(~r/^<@[A-Z0-9].+?>$/, text) do
+      true ->
+        "<@#{user}>?"
+
+      _ ->
+        String.replace(text, ~r/<@[A-Z0-9].+?>/, "")
+        |> String.trim()
+        |> get_response()
+    end
   end
 
-  @impl true
-  def handle_info({:DOWN, _ref, :process, _pid, status}, nil) do
-    IO.puts("Reference exited with status: #{status}")
-    {:noreply, nil}
+  defp get_response("your source"), do: "https://github.com/acdibble/code-bro-bot"
+  defp get_response("ping"), do: "pong"
+  defp get_response("version"), do: "elixir"
+  defp get_response("coronavirus update"), do: get_report(0)
+  defp get_response(_), do: "I don't know what to do with my hands"
+
+  defp get_report(10), do: "Unable to get results"
+
+  defp get_report(days_back)
+       when is_integer(days_back) and days_back < 10 do
+    case HTTPoison.get(get_url(days_back)) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        String.split(body, "\n") |> parse_report(%{}) |> IO.inspect()
+        "parsed"
+
+      _ ->
+        get_report(days_back + 1)
+    end
+  end
+
+  defp get_url(days_back) do
+    [year, month, day] =
+      Date.utc_today()
+      |> Date.add(-1 * days_back)
+      |> Date.to_string()
+      |> String.split("-")
+
+    base_url() <> Enum.join([month, day, year], "-") <> ".csv"
+  end
+
+  defp base_url do
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
+  end
+
+  defp parse_report([head | tail], map) do
+    updated =
+      if String.contains?(head, ",US,") do
+        IO.puts("#{head}: #{String.ends_with?(head, ", US\"")}")
+        parse_line(head, map)
+      else
+        map
+      end
+
+    parse_report(tail, updated)
+  end
+
+  defp parse_report([], map), do: generate_report(map)
+
+  defp parse_line(line, map) do
+    parts = String.split(line, ",")
+    state = Enum.at(parts, 2)
+    last_reported = Enum.at(parts, 4)
+
+    [confirmed, deaths, recovered, active] =
+      Enum.map([7, 8, 9, 10], &(Enum.at(parts, &1) |> String.to_integer()))
+
+    Map.merge(
+      map,
+      %{
+        state => %{
+          "last_reported" => last_reported |> String.split(" ") |> List.first(),
+          "confirmed" => confirmed,
+          "deaths" => deaths,
+          "recovered" => recovered,
+          "active" => active
+        }
+      },
+      &merge_helper/3
+    )
+  end
+
+  defp merge_helper(_, a, b) when is_map(a) and is_map(b), do: Map.merge(a, b, &merge_helper/3)
+  defp merge_helper(_, a, b) when is_integer(a) and is_integer(b), do: a + b
+  defp merge_helper(_, a, b), do: if(a < b, do: a, else: b)
+
+  defp generate_report(map) do
+    Enum.map(map, fn {state, data} ->
+      nil
+    end)
   end
 end
--- lib/code_bro_bot/slack/request.ex
+++ lib/code_bro_bot/slack/request.ex
@@ -1,22 +1,12 @@
 defmodule CodeBroBot.Slack.Request do
   def call(body) do
-    {:ok, conn} = Mint.HTTP.connect(:https, "slack.com", 443)
-
-    {:ok, conn, _ref} =
-      Mint.HTTP.request(conn, "POST", "/api/chat.postMessage", req_headers(), Jason.encode!(body))
-
-    receive do
-      message ->
-        case Mint.HTTP.stream(conn, message) do
-          {:ok, _conn, responses} -> responses
-        end
-    end
-  end
-
-  defp req_headers do
-    [
-      {"content-type", "application/json;charset=utf8"},
-      {"authorization", "Bearer #{System.get_env("SLACK_OAUTH_TOKEN")}"}
-    ]
+    HTTPoison.post(
+      "https://slack.com/api/chat.postMessage",
+      Jason.encode!(body),
+      [
+        {"content-type", "application/json;charset=utf8"},
+        {"authorization", "Bearer #{System.get_env("SLACK_OAUTH_TOKEN")}"}
+      ]
+    )
   end
 end
