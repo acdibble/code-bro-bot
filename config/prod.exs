64a285f6ee143224980cb0ac463bc3a13ff7a25d
Andrew Dibble <andrew@dibble.codes>
2020-05-08 22:33:16 +0200

    Parse string to integer

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ import Config
 
 config(
   :code_bro_bot,
-  port: System.fetch_env!("PORT"),
+  port: System.fetch_env!("PORT") |> String.to_integer(),
   slack_signing_secret: System.fetch_env!("SLACK_SIGNING_SECRET"),
   slack_oauth_token: System.fetch_env!("SLACK_OAUTH_TOKEN")
 )
